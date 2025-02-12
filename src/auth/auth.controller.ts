5ff3dc6efa8f74ac96ed9c24f8d5a1a1cb205cf5
Emil <mikayelyanemil19@gmail.com>
2025-02-12 17:09:34 +0400

    feat: add signup auth controller route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,18 +4,21 @@ import { TokensService } from 'src/tokens/tokens.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
     constructor(
         private usersService: UsersService,
-        private authService: AuthService
+        private authService: AuthService,
+        private configService: ConfigService
     ) { }
 
     @Post('signup')
     async signup(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
         const { id, email } = await this.usersService.create(createUserDto);
         const { accessToken, refreshToken } = await this.authService.generateTokens({ sub: id, email });
-        res.status(HttpStatus.CREATED).json({ id, email, accessToken, refreshToken });
+        res.cookie('refresh_token', refreshToken, { maxAge: this.configService.get('COOKIE_MAX_AGE'), httpOnly: true });
+        res.status(HttpStatus.CREATED).json({ accessToken });
     }
 }
