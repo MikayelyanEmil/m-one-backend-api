3fb0376b8fd75d098bf0a29f00f87979aae6f2fc
Emil <mikayelyanemil19@gmail.com>
2025-02-12 18:58:27 +0400

    feat: add friend-requests module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,14 @@ import { AuthModule } from './auth/auth.module';
 import { TokensModule } from './tokens/tokens.module';
 import { PgClientService } from './pg-client.service';
 import { ConfigModule } from '@nestjs/config';
+import { FriendRequestsModule } from './friend-requests/friend-requests.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    UsersModule, AuthModule, TokensModule],
+    UsersModule, AuthModule, TokensModule, FriendRequestsModule],
   controllers: [AppController],
   providers: [AppService, PgClientService],
 })
